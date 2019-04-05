50f5334fbe98f757357829afc18ad9b11c1c86f9
Sanat Sharma <sanatcolumban@gmail.com>
2019-04-05 16:07:18 -0500

    Add structure and bert example

---
+++ specdial.py
@@ -0,0 +1,9 @@
+# Main file for extracting Visdial features and predicting specificity
+
+import argparse
+import collections
+import logging
+import json
+
+
+# Retrieve example information to feed into BERT
